757897d61e0fa4d5807834e6a7ff5e31d010f68b
fabioguelfi <fabioguelfiunix@gmail.com>
2018-06-03 02:34:40 -0300

    signin login logout components

--- src/app/store/app.states.ts
+++ src/app/store/app.states.ts
@@ -1,7 +1,21 @@
 import { createFeatureSelector } from '@ngrx/store';
-
 import * as auth from './reducers/auth.reducers';
+import { User } from '../models/user';
 
+export interface State {
+  // is a user authenticated?
+  isAuthenticated: boolean;
+  // if authenticated, there should be a user object
+  user: User | null;
+  // error message
+  errorMessage: string | null;
+}
+
+export const initialState: State = {
+  isAuthenticated: false,
+  user: null,
+  errorMessage: null
+};
 
 export interface AppState {
   authState: auth.State;

44c557d3e66c3d1524725dbecbbf4136737fb218
fabioguelfi <fabioguelfiunix@gmail.com>
2018-06-03 02:38:22 -0300

    update scheme html components

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { AppState } from '../../store/app.states';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+import { AppState, selectAuthState } from '../../store/app.states';
+import { LogOut } from '../../store/actions/auth.actions';
+
 
 @Component({
   selector: 'app-landing',
@@ -9,11 +13,23 @@ import { Store } from '@ngrx/store';
 })
 export class LandingComponent implements OnInit {
 
+  getState: Observable<any>;
+  isAuthenticated: false;
+  user = null;
+  errorMessage = null;
+
   constructor(
     private store: Store<AppState>
-  ) { }
+  ) {
+    this.getState = this.store.select(selectAuthState);
+  }
 
   ngOnInit() {
+    this.getState.subscribe((state) => {
+      this.isAuthenticated = state.isAuthenticated;
+      this.user = state.user;
+      this.errorMessage = state.errorMessage;
+    });
   }
 
   logOut(): void {

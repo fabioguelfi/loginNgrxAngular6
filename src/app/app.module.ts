ff77533bd9f3a37b5ef490bacf88d529518d38bc
fabioguelfi <fabioguelfiunix@gmail.com>
2018-06-02 23:09:14 -0300

    sign in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
@@ -17,6 +18,7 @@ import { ROUTES } from './app.routes';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [],
